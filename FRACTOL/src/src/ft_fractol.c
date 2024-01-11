a06b87391734644bfb73c272a474b9395548a1c1
Descamil <73858568+Daniel404kali@users.noreply.github.com>
2024-01-11 18:16:06 +0100

    Add files via upload

--- FRACTOL/src/src/ft_fractol.c
+++ FRACTOL/src/src/ft_fractol.c
@@ -6,7 +6,7 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 19:05:06 by descamil          #+#    #+#             */
-/*   Updated: 2024/01/11 16:55:46 by descamil         ###   ########.fr       */
+/*   Updated: 2024/01/11 18:02:51 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,13 @@ int	ft_argc_4(t_data *img, char *argv2, char *argv3)
 	if ((img->c_julia_r < -2.0 || img->c_julia_r > 2.0)
 		|| (img->c_julia_i < -2.0 || img->c_julia_i > 2.0))
 	{
-		write(1, "Write: julia '-2.0 <-> 2.0' '-2.0 <-> 2.0'\n", 44);
+		write(1, "Write: julia '-2.0 <-> 2.0' '-2.0 <-> 2.0'\n", 43);
+		write(1, "Recomended: \n", 13);
+		write(1, "--> [-0.1\t 0.87]\n", 18);
+		write(1, "--> [ 0.285\t-0.01]\n", 20);
+		write(1, "--> [-0.72\t-0.196]\n", 20);
+		write(1, "--> [-0.51\t-0.601]\n", 20);
+		write(1, "--> [-1.3\t 0.00525]\n", 21);
 		return (0);
 	}
 	return (1);
@@ -43,12 +49,13 @@ int	main(int argc, char *argv[])
 	{
 		if (ft_argc_4(&img, argv[2], argv[3]) == 0)
 			return (0);
-	}
-	else if ((ft_strncmp(img.name, "mandel", 7) != 0)
+	}	
+	else if (((ft_strncmp(img.name, "mandel", 7) != 0)
 		&& (ft_strncmp(img.name, "julia", 6) != 0)
 		&& (ft_strncmp(img.name, "bonus", 6) != 0))
+		|| argc == 3 || argc > 4)
 	{
-		write(1, "Write: 'mandel' o 'julia' o 'bonus'\n", 37);
+		write(1, "Write: 'mandel' o 'julia [x y]' o 'bonus'\n", 43);
 		return (0);
 	}
 	ft_create_window(&img);
